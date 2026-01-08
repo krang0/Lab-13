7e5dd132b0f5838124698f4a851179d7f9753873
Abdullah Arslan <147412768+krang0@users.noreply.github.com>
2026-01-08 13:19:59 +0300

    feat: setup navigation for lab app

---
+++ screens/CameraScreen.js
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function CameraScreen() {
+  return <View><Text>Camera Screen</Text></View>;
+}
---
+++ screens/LocationScreen.js
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function LocationScreen() {
+  return <View><Text>Location Screen</Text></View>;
+}
